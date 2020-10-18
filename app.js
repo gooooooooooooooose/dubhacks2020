0d1013cb1b4475cdf430e53224b78882df737cf2
Mitchell Massie <mitchellm1618@gmail.com>
2020-10-18 00:05:03 -0700

    saving changes top-level app.js, will likely be overwritten

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("front/public"));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
